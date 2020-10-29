96f631e850565f5b41dcccc2e4157b69fa99d2b8
julieta <julietaorce@gmail.com>
2020-10-29 16:22:05 +0000

    Issue-11-17 Cascading conditional renderings

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -69,6 +69,32 @@ const Characters = ({ filter, query, dataAttribute, onSeletedTypeName }: Charact
   };
   const handleClose = () => setShow(false);
 
+  const handleContentItem = (elem: itemInterface) => {
+    if (elem.image) {
+      return (
+        <>
+          <img src={elem.image} alt={elem.name + "image"} className=" w-100" />
+          <h4 className="modal-title">{elem.name}</h4>
+        </>
+      )
+    } else if (elem.dimension) {
+      return (
+        <>
+          <h4 className="modal-title">{elem.name}</h4>
+          <p>Type: <h6 className="d-inline">{elem.type}</h6></p>
+          <p>Dimension: <h6 className="d-inline">{elem.dimension}</h6></p>
+        </>
+      )
+    }
+    return (
+      <>
+        <h4 className="modal-title">{elem.name}</h4>
+        <p>Episode Date: <h6 className="d-inline">{elem.air_date}</h6></p>
+        <p>Episode: <h6 className="d-inline">{elem.episode}</h6></p>
+      </>
+    )
+  }
+
   return (
     <div className="mb-5" >
       <Entity show={show} handleClose={handleClose} selElem={selectedElem} />
@@ -76,24 +102,7 @@ const Characters = ({ filter, query, dataAttribute, onSeletedTypeName }: Charact
         {data[dataAttribute].results.map((elem: itemInterface) => (
           <div onClick={() => handleShow(elem)} className="col-12 col-md-6 col-lg-4 col-xl-3" key={elem.id} >
             <div className="contentItem hoverShadow">
-              {elem.image ?
-                <>
-                  <img src={elem.image} alt={elem.name + "image"} className=" w-100" />
-                  <h4 className="modal-title">{elem.name}</h4>
-                </>
-                : elem.dimension ?
-                  <>
-                    <h4 className="modal-title">{elem.name}</h4>
-                    <p>Type: <h6 className="d-inline">{elem.type}</h6></p>
-                    <p>Dimension: <h6 className="d-inline">{elem.dimension}</h6></p>
-                  </>
-                  :
-                  <>
-                    <h4 className="modal-title">{elem.name}</h4>
-                    <p>Episode Date: <h6 className="d-inline">{elem.air_date}</h6></p>
-                    <p>Episode: <h6 className="d-inline">{elem.episode}</h6></p>
-                  </>
-              }
+              {handleContentItem(elem)}
             </div>
           </div>
         ))}
--- src/components/Entity.tsx
+++ src/components/Entity.tsx
@@ -8,90 +8,99 @@ interface EntityPropsInterface {
   selElem: itemInterface
 }
 
-const Entity = ({ show, handleClose, selElem }: EntityPropsInterface) => (
-  <Modal show={show} onHide={handleClose}>
-    {
-      selElem.name ?
-        <>
-          <div className="d-flex justify-content-end">
-            <button onClick={handleClose} className="close closeBtn">X</button>
-          </div>
-          {selElem.image ?
-            <Modal.Body>
-              <img width="100%" src={selElem.image} alt={selElem.name} />
-              <Modal.Title className="titleModal">
-                {selElem.name}
-              </Modal.Title>
-              <table>
-                <tbody>
-                  <tr>
-                    <td>Type:</td>
-                    <td><strong>{selElem.type === "" ? " - " : selElem.type}</strong></td>
-                  </tr>
-                  <tr>
-                    <td>Gender:</td>
-                    <td><strong>{selElem.gender}</strong></td>
-                  </tr>
-                  <tr>
-                    <td>Species:</td>
-                    <td><strong>{selElem.species}</strong></td>
-                  </tr>
-                </tbody>
-              </table>
-            </Modal.Body>
-            : selElem.dimension ?
-              <Modal.Body>
-                <Modal.Title className="titleModal">
-                  {selElem.name}
-                </Modal.Title>
-                <p><strong><u>Type:</u></strong>{" " + selElem.type}</p>
-                <p><strong><u>Dimension:</u></strong>{" " + selElem.dimension}</p>
-                <p><u><strong>Residents: </strong></u></p>
-                <div className="row">
-                  {selElem.residents && selElem.residents.map((elem: itemInterface, i: number) => {
-                    if (i < 5) {
-                      if (elem.name == null) { return <strong className="noData">No Residents</strong> }
-                      return (
-                        <div key={elem.id} className="col-5 col-md-3 titleModal contentItem">
-                          <img width="100%" src={elem.image} alt={elem.name} /> <br />
-                          <strong>{" " + elem.name}</strong>
-                        </div>
-                      )
-                    }
-                    return null
-                  }
-                  )
-                  }
-                </div>
-              </Modal.Body>
-              : <Modal.Body>
-                <Modal.Title className="titleModal">
-                  {selElem.name}
-                </Modal.Title>
-                <p><strong><u>Release Date:</u></strong>{" " + selElem.air_date}</p>
-                <p><strong><u>Episode:</u></strong>{" " + selElem.episode}</p>
-                <p><u><strong>Characters: </strong></u></p>
-                <div className="row">
-                  {selElem.characters && selElem.characters.map((elem: itemInterface, i: number) => {
-                    if (i < 5) {
-                      return (
-                        <div key={elem.id} className="col-5 col-md-3 titleModal contentItem">
-                          <img width="100%" src={elem.image} alt={elem.name} /> <br />
-                          <strong>{" " + elem.name}</strong>
-                        </div>
-                      )
-                    }
-                    return null
-                  }
-                  )
-                  }
-                </div>
+const Entity = ({ show, handleClose, selElem }: EntityPropsInterface) => {
+  if (show && !selElem.name) {
+    return <>Loading...</>
+  }
 
-              </Modal.Body>
+  const handleBody = () => {
+    if (selElem.image) {
+      return (
+        <Modal.Body>
+          <img width="100%" src={selElem.image} alt={selElem.name} />
+          <Modal.Title className="titleModal">
+            {selElem.name}
+          </Modal.Title>
+          <table>
+            <tbody>
+              <tr>
+                <td>Type:</td>
+                <td><strong>{selElem.type === "" ? " - " : selElem.type}</strong></td>
+              </tr>
+              <tr>
+                <td>Gender:</td>
+                <td><strong>{selElem.gender}</strong></td>
+              </tr>
+              <tr>
+                <td>Species:</td>
+                <td><strong>{selElem.species}</strong></td>
+              </tr>
+            </tbody>
+          </table>
+        </Modal.Body>
+      )
+    } else if (selElem.dimension) {
+      return (
+        <Modal.Body>
+          <Modal.Title className="titleModal">
+            {selElem.name}
+          </Modal.Title>
+          <p><strong><u>Type:</u></strong>{" " + selElem.type}</p>
+          <p><strong><u>Dimension:</u></strong>{" " + selElem.dimension}</p>
+          <p><u><strong>Residents: </strong></u></p>
+          <div className="row">
+            {selElem.residents && selElem.residents.map((elem: itemInterface, i: number) => {
+              if (i < 5) {
+                if (elem.name == null) { return <strong className="noData">No Residents</strong> }
+                return (
+                  <div key={elem.id} className="col-5 col-md-3 titleModal contentItem">
+                    <img width="100%" src={elem.image} alt={elem.name} /> <br />
+                    <strong>{" " + elem.name}</strong>
+                  </div>
+                )
+              }
+              return null
+            })}
+          </div>
+        </Modal.Body>
+      )
+    }
+    return (
+      <Modal.Body>
+        <Modal.Title className="titleModal">
+          {selElem.name}
+        </Modal.Title>
+        <p><strong><u>Release Date:</u></strong>{" " + selElem.air_date}</p>
+        <p><strong><u>Episode:</u></strong>{" " + selElem.episode}</p>
+        <p><u><strong>Characters: </strong></u></p>
+        <div className="row">
+          {selElem.characters && selElem.characters.map((elem: itemInterface, i: number) => {
+            if (i < 5) {
+              return (
+                <div key={elem.id} className="col-5 col-md-3 titleModal contentItem">
+                  <img width="100%" src={elem.image} alt={elem.name} /> <br />
+                  <strong>{" " + elem.name}</strong>
+                </div>
+              )
+            }
+            return null
           }
-        </>
-        : "Loading"}
-  </Modal>
-)
+          )
+          }
+        </div>
+      </Modal.Body>
+    )
+  }
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <div className="d-flex justify-content-end">
+        <button onClick={handleClose} className="close closeBtn">X</button>
+      </div>
+      {handleBody()}
+    </Modal>
+  )
+}
+
 
 export default Entity;
