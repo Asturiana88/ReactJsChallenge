baf38f24dea4a4f8bf02641d06303565d22f4cbb
julieta <julietaorce@gmail.com>
2020-10-29 15:28:20 +0000

    Issue-10 Usage of implicit return

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -73,32 +73,30 @@ const Characters = ({ filter, query, dataAttribute, onSeletedTypeName }: Charact
     <div className="mb-5" >
       <Entity show={show} handleClose={handleClose} selElem={selectedElem} />
       <div className="row">
-        {data[dataAttribute].results.map((elem: itemInterface) => {
-          return (
-            <div onClick={() => handleShow(elem)} className="col-12 col-md-6 col-lg-4 col-xl-3" key={elem.id} >
-              <div className="contentItem hoverShadow">
-                {elem.image ?
+        {data[dataAttribute].results.map((elem: itemInterface) => (
+          <div onClick={() => handleShow(elem)} className="col-12 col-md-6 col-lg-4 col-xl-3" key={elem.id} >
+            <div className="contentItem hoverShadow">
+              {elem.image ?
+                <>
+                  <img src={elem.image} alt={elem.name + "image"} className=" w-100" />
+                  <h4 className="modal-title">{elem.name}</h4>
+                </>
+                : elem.dimension ?
                   <>
-                    <img src={elem.image} alt={elem.name + "image"} className=" w-100" />
                     <h4 className="modal-title">{elem.name}</h4>
+                    <p>Type: <h6 className="d-inline">{elem.type}</h6></p>
+                    <p>Dimension: <h6 className="d-inline">{elem.dimension}</h6></p>
                   </>
-                  : elem.dimension ?
-                    <>
-                      <h4 className="modal-title">{elem.name}</h4>
-                      <p>Type: <h6 className="d-inline">{elem.type}</h6></p>
-                      <p>Dimension: <h6 className="d-inline">{elem.dimension}</h6></p>
-                    </>
-                    :
-                    <>
-                      <h4 className="modal-title">{elem.name}</h4>
-                      <p>Episode Date: <h6 className="d-inline">{elem.air_date}</h6></p>
-                      <p>Episode: <h6 className="d-inline">{elem.episode}</h6></p>
-                    </>
-                }
-              </div>
+                  :
+                  <>
+                    <h4 className="modal-title">{elem.name}</h4>
+                    <p>Episode Date: <h6 className="d-inline">{elem.air_date}</h6></p>
+                    <p>Episode: <h6 className="d-inline">{elem.episode}</h6></p>
+                  </>
+              }
             </div>
-          )
-        })}
+          </div>
+        ))}
       </div>
       <button className="btn btn-dark ml-1" disabled={prevPage == null} onClick={() => setPage(prevPage)}>Previous Page</button>
       <button className="btn btn-dark ml-1" disabled={nextPage == null} onClick={() => setPage(nextPage)}>Next Page</button>
--- src/components/SideFilters.tsx
+++ src/components/SideFilters.tsx
@@ -5,47 +5,45 @@ interface SideFiltersPropsInterface {
     value: string
 }
 
-const SideFilters = ({ handler, value }: SideFiltersPropsInterface) => {
-    return (
-        <div className="c-sidebar c-sidebar-dark c-sidebar-show">
-            <div className="d-flex flex-column">
-                <h4>Filters</h4>
-                <label>
-                    <input
-                        style={{ marginRight: 10 }}
-                        type="radio"
-                        value="characters"
-                        name="filter"
-                        onChange={handler}
-                        checked={value === "characters"}
-                    />
+const SideFilters = ({ handler, value }: SideFiltersPropsInterface) => (
+    <div className="c-sidebar c-sidebar-dark c-sidebar-show">
+        <div className="d-flex flex-column">
+            <h4>Filters</h4>
+            <label>
+                <input
+                    style={{ marginRight: 10 }}
+                    type="radio"
+                    value="characters"
+                    name="filter"
+                    onChange={handler}
+                    checked={value === "characters"}
+                />
                     Characters
                 </label>
-                <label>
-                    <input
-                        style={{ marginRight: 10 }}
-                        type="radio"
-                        value="episodes"
-                        name="filter"
-                        onChange={handler}
-                        checked={value === "episodes"}
-                    />
+            <label>
+                <input
+                    style={{ marginRight: 10 }}
+                    type="radio"
+                    value="episodes"
+                    name="filter"
+                    onChange={handler}
+                    checked={value === "episodes"}
+                />
                         Episodes
                 </label>
-                <label>
-                    <input
-                        style={{ marginRight: 10 }}
-                        type="radio"
-                        value="locations"
-                        name="filter"
-                        onChange={handler}
-                        checked={value === "locations"}
-                    />
+            <label>
+                <input
+                    style={{ marginRight: 10 }}
+                    type="radio"
+                    value="locations"
+                    name="filter"
+                    onChange={handler}
+                    checked={value === "locations"}
+                />
                     Locations
                 </label>
-            </div>
         </div>
-    )
-}
+    </div>
+)
 
 export default SideFilters;
